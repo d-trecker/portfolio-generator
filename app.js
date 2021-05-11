d2f249eaeaebdf775f0b58df12b7c62395c1b053
Dylan Trecker <dylantrecker@WarMachineAir.local>
2021-05-11 00:57:14 -0700

    added inquirer

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+.then(answers => console.log(answers));
 
-const profileDataArgs= process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-console.log(name, github);
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
 //----------NOTES ----------
@@ -63,3 +67,23 @@ fs.writeFile('./index.html', pageHTML, err => {
   
 //     console.log('Portfolio complete! Check out index.html to see the output!');
 //   });
+
+
+//-------------------Notes Before inquirer
+// const fs = require('fs');
+
+// const generatePage = require('./src/page-template.js');
+
+// const profileDataArgs= process.argv.slice(2);
+
+// const [name, github] = profileDataArgs;
+
+// console.log(name, github);
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
